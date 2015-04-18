9f8001ef35b36b2de818c7ebd99756ec14ec1ca9
Mike Moss <mikemoss@mikemoss-mac-book.local>
2015-04-18 14:23:00 -0700

    horse racing started

--- js/leader-board.js
+++ js/leader-board.js
@@ -1,7 +1,7 @@
 var app = angular.module('leaderboard', ['firebase', 'ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider){
-  $urlRouterProvider.otherwise('/main');
+  $urlRouterProvider.otherwise('/');
 
   $stateProvider
     .state('main', {
@@ -16,7 +16,7 @@ app.config(function($stateProvider, $urlRouterProvider){
 
 app.constant('FIREBASE_URI', 'https://horse-race.firebaseio.com/');
 
-app.controller('MainCtrl', function (ContestantsService) {
+app.controller('MainCtrl', function(ContestantsService, $scope, $interval) {
     var main = this;
     main.newContestant = {lane: '', name: '', score: ''};
     main.currentContestant = null;
@@ -43,6 +43,29 @@ app.controller('MainCtrl', function (ContestantsService) {
     main.decrementScore = function () {
         main.currentContestant.score = parseInt(main.currentContestant.score, 10) - 1;
         main.updateContestant(main.currentContestant);
+    };
+
+    $scope.start = function(){
+      var contestants = ContestantsService.getContestants();
+      contestants.forEach(function(contestant){
+         $interval(function(){
+          contestant.score = parseFloat(contestant.score);
+          contestant.score += Math.floor(Math.random() * 10);
+
+          }, 1000, 10);
+      });
+    };
+
+    $scope.winner = function(){
+      var winner = {score: 0};
+      var contestants = ContestantsService.getContestants();
+      contestants.forEach(function(contestant){
+        if(contestant.score > winner.score){
+          winner = contestant;
+        }
+      });
+      swal(winner.name, "You won!", "success")
+
     };
 });
 
